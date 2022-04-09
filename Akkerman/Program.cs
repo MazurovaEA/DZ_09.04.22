d495060336f8a44cda17bf2ce242679f8f888744
Katerina <ealmazurova@internet.ru>
2022-04-09 12:16:57 +0300

    Функция Аккермана

---
+++ Akkerman/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 3: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n. */
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int Akkerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akkerman(n - 1, 1);
+    return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+int N = Promt("Введите n => ");
+int M = Promt("Введите m => ");
+Console.Write(Akkerman(N,M));
