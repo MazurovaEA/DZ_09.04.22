f69351f29312e2ded3130b0ab6509eeaf31cb114
Katerina <ealmazurova@internet.ru>
2022-04-09 12:17:24 +0300

    Вывод четных элементов между числами

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, 
+которая выведет все чётные натуральные числа в промежутке от M до N.*/
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+void PrintEvenNumbers(int A, int B)
+{
+    if (A > B) return;
+    if (A % 2 == 0) Console.Write(A+" ");
+    PrintEvenNumbers(A + 1, B);
+}
+
+int A = Promt("Введите A => ");
+int B = Promt("Введите B => ");
+PrintEvenNumbers(A, B);
