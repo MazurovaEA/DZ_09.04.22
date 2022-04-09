e9d682fa6488da365ac549f0e054e05b53d9f72a
Katerina <ealmazurova@internet.ru>
2022-04-09 12:16:38 +0300

    Сумма натуральных элементов от M до N

---
+++ SumElem/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 2: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N.*/
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int PrintN(int A, int B)
+{
+    if (A == B) return A;
+    return A + PrintN(A + 1, B);
+}
+
+int N = Promt("Введите N => ");
+int M = Promt("Введите M => ");
+Console.WriteLine($"Сумма чисел от {N} до {M} равна {PrintN(N, M)}");
